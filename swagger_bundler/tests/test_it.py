6d66da7e070df21827cccd4564224e51c1969e13
podhmo <ababjam61+github@gmail.com>
2016-09-25 00:06:03 +0900

    add test fix #1

--- swagger_bundler/tests/test_it.py
+++ swagger_bundler/tests/test_it.py
@@ -20,3 +20,18 @@ class GenerationgTests(unittest.TestCase):
         with open(os.path.join(here, "data/xproduct.yaml")) as rf:
             expected = yaml.load(rf)
         self.assertEqual(result, expected)
+
+    def test_it__duplicated_import(self):
+        # dependencies:
+        # user -> {group, common}
+        # group -> {common}
+
+        from swagger_bundler import make_rootcontext
+
+        ctx = make_rootcontext()
+        with open(os.path.join(here, "data/parts/user.parts.yaml")) as rf:
+            subcontext = ctx.make_subcontext_from_port(rf)
+            result = self._callFUT(subcontext, subcontext.data)
+        with open(os.path.join(here, "data/yuser.yaml")) as rf:
+            expected = yaml.load(rf)
+        self.assertEqual(result, expected)
