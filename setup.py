d38a3329d63833086b688dcff93402c9e3cecfea
podhmo <ababjam61+github@gmail.com>
2016-10-26 14:24:28 +0900

    bump version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ watch_extras = [
 ]
 
 setup(name='swagger-bundler',
-      version='0.1.6',
+      version='0.1.7',
       description='swagger schema bundler',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
