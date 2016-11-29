d1fe06e13bc7031ce9383124d951bb7926d2afc6
podhmo <ababjam61+github@gmail.com>
2016-11-30 05:48:15 +0900

    byebye bundling module

--- swagger_bundler/drivers/fileconcat.py
+++ swagger_bundler/drivers/fileconcat.py
@@ -1,18 +1,36 @@
-from ..modifiers import bundling
+# -*- coding:utf-8 -*-
+import logging
+from ..modifiers import composing
+from ..modifiers import prefixing
 from ..modifiers.ordering import ordering
 from ..modifiers import orphancheck
 from .. import loading
+logger = logging.getLogger(__name__)
 
 
 class FileConcatDriver:
-    def transform(self, ctx, data, namespace=None):
-        return bundling.transform(ctx, data, namespace=namespace)
+    def transform(self, ctx, data, namespace=None, last=False):
+        subfiles = ctx.detector.detect_compose()
+        if subfiles:
+            data = composing.transform(ctx, data, subfiles, last=last)
+
+        namespace = namespace or ctx.detector.detect_namespace()
+        if namespace:
+            data = prefixing.transform(ctx, data, namespace=namespace, last=last)
+
+        # TODO: handling code
+        postscript = ctx.options["postscript_hook"].get("bundle")
+        if postscript and callable(postscript):
+            postscript_result = postscript(ctx, data, namespace=namespace, last=last)
+            if postscript_result is not None:
+                data = postscript_result
+        return data
 
     def run(self, ctx, inp, outp, namespace=None):
         subcontext = ctx.make_subcontext_from_port(inp)
-        result = bundling.transform(subcontext, subcontext.data, namespace=namespace, last=True)
+        result = self.transform(subcontext, subcontext.data, namespace=namespace, last=True)
 
-        orphancheck.check_orphan_reference(self.ctx, result, exception_on_fail=False)
+        orphancheck.check_orphan_reference(ctx, result, exception_on_fail=False)
 
         ordered = ordering(result)
         loading.dump(ordered, outp)
